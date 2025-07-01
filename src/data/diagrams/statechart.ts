0d94ee1e0f7c37b613b2cbac36639c40826d1492
Jian Liew <jianloongliew@gmail.com>
2025-07-01 19:53:58 +1000

    Restore [*] start node for all statechart diagrams

--- src/data/diagrams/statechart.ts
+++ src/data/diagrams/statechart.ts
@@ -18,67 +18,28 @@ Pending --> [*]
 
 export const noPlacedState = `stateDiagram-v2
 [*] --> Pending: Customer places an order
-    note left of Pending
-        • Item must be in stock
-        • Item purchased must not exceed inventory
-        • Item purchased must NOT be deducted yet
-    end note
 Pending --> [*]
 `;
 
 export const businessRules = `stateDiagram-v2
 [*] --> Pending: Customer places an order
-    note left of Pending
-        • Items must be in stock
-        • Items purchased must not exceed inventory
-        • Items purchased must not be deducted yet
-        • Items purchased must not be above the limit set
-    end note
 Pending-->Paid: Customer pays for the order
-        note right of Paid
-        • Payment must be successful
-        • Items must still be in stock
-        • Items are now deducted
-    end note
 Paid --> [*]
 `;
 
 export const reverseTransitions = `stateDiagram-v2
 [*] --> Pending: Customer places an order
-    note left of Pending
-        • Items must be in stock
-        • Items purchased must not exceed inventory
-        • Items purchased must not be deducted yet
-        • Items purchased must not be above the limit set
-    end note
 Pending-->Paid: Customer pays for the order
-        note right of Paid
-        • Payment must be successful
-        • Items must still be in stock
-        • Items are now deducted
-    end note
 Paid --> Pending: Item no longer in stock, customer preference change.
 Paid --> [*]
 `;
 
 export const cancelledState = `stateDiagram-v2
+[*] --> Pending : Customer places an order
+Pending-->Paid: Customer pays for the order
+Paid --> Cancelled: [Guards]
+Pending --> Cancelled: [Guards]
 Pending --> [*]
 Paid --> [*]
 Cancelled --> [*]
-
-note right of Paid
-        • Payment must be successful
-        • Items must still be in stock
-        • Items are now deducted
-end note
-note left of Pending
-        • Items must be in stock
-        • Items purchased must not exceed inventory
-        • Items purchased must not be deducted yet
-        • Items purchased must not be above the limit set
-end note
-note left of Cancelled
-        • Payment not made within a certain number of days
-        • Cancelled as per customer request
-end note
 `; 
